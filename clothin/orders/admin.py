f1af3775edb7ea12b650f8e08f943d4af387166d
nurmadi <nurgali.nurmadi@gmail.com>
2026-06-23 15:18:51 +0500

    Fixed Stripe bug

--- clothin/orders/admin.py
+++ clothin/orders/admin.py
@@ -10,6 +10,6 @@ class OrderItemInline(admin.TabularInline):
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email', 'address',
-                    'postal_code', 'city', 'paid', 'created', 'updated']
+                    'postal_code', 'city', 'paid', 'stripe_id', 'created', 'updated']
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
